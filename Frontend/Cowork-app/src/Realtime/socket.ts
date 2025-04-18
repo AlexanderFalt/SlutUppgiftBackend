57a9716e878bdc43849741b6e44e1576fbcbf540
AlexanderFalt <faltskog.alexander@gmail.com>
2025-04-18 17:26:03 +0200

    Fixed socket, maybe 2.

--- Frontend/Cowork-app/src/Realtime/socket.ts
+++ Frontend/Cowork-app/src/Realtime/socket.ts
@@ -1,6 +1,7 @@
 import { io } from "socket.io-client";
 
-export const socket = io("http://localhost:8080", {
+const API = import.meta.env.VITE_API_URL || "";  
+export const socket = io(API, {
   withCredentials: true,
   autoConnect: false,
   reconnection: true,
